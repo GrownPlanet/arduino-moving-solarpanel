12a4eb3acad28f45a0914c488ffb6bcd4e9f8726
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2024-02-05 11:49:26 +0100

    add comments and improve some variable names

--- low_high.ino
+++ low_high.ino
@@ -4,75 +4,61 @@ const int servcount = 4;
 Servo servos[servcount];
 const int servopins[servcount] = {8, 9, 10, 11};
 
-const int senservcount = 4;
-const int lichtsenspins[senservcount] = {A0, A1, A2, A3};
+const int ldr_count = 4;
+const int ldr_pins[ldr_count] = {A0, A1, A2, A3};
 int array_in_array[servcount][2];
 
+// the heights the motors will be set to
 int heights[3] = {
   0, 90, 180
 };
-const int opposites[servcount][2] {
+
+// which LDR is oppesite to which LDR ?
+const int opposites[servcount/2][2] {
   {0, 3},
   {1, 2},
-  {2, 1},
-  {3, 0},
 };
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello, World!");
 
+  // activate the servo's and make all the positions 0 
   for (int i = 0; i < servcount; i++) {
     servos[i].attach(servopins[i]);
     servos[i].write(0);
   }
-  for (int i = 0; i < senservcount; i++) {
-    pinMode(lichtsenspins[i], INPUT);
+  // set all the ldr's to input
+  for (int i = 0; i < ldr_count; i++) {
+    pinMode(ldr_pins[i], INPUT);
   }
 }
 
 void loop() {
-  return;
-  for (int i = 0; i < senservcount; i++) {
+  // read the data from the LDR's
+  for (int i = 0; i < ldr_count; i++) {
     array_in_array[i][0] = i;
-    array_in_array[i][1] = analogRead(lichtsenspins[i]);
+    array_in_array[i][1] = analogRead(ldr_pins[i]);
   }
   // if we want to have more specific height values
   // quicksort(array_in_array, 0, servcount - 1);
 
-  int i = highest_element_i(array_in_array);
+  // but for now we are going to use a more primative way
+  int i = highest_element_index(array_in_array);
   Serial.println(i);
-  // int i = 1;
 
-  servos[i].write(heights[0]);
-
-  switch (i) {
-    case 0:
-    case 3:
-    servos[1].write(heights[1]);
-    servos[2].write(heights[1]);
-    break;
-    case 1:
-    case 2:
-    servos[0].write(heights[1]);
-    servos[3].write(heights[1]);
-    break;
-  }
-
-  int opposite_i = 0;
-  for (auto v: opposites) {
-    if (i == opposites[i][0]) {
-      opposite_i = opposites[i][1];
-    }
-  }
-  servos[opposite_i ].write(heights[2]);
-
+  // set the motors to the right position 
+  set_motors(i);
+  
+  // a bit of debuging output
   printarray();
-
-  delay(1000);
   Serial.println();
+
+  // don't overheat the arduino
+  delay(1000);
 }
 
+// nice formatting for printing the debug information
 void printarray() {
   for (auto a: array_in_array) {
     Serial.print(a[0]);
@@ -81,12 +67,43 @@ void printarray() {
   }
 }
 
+void set_motors(int i) {
+  // set the motor closest to the light to the slowest stand
+  servos[i].write(heights[0]);
 
-int highest_element_i(int arr[senservcount][2]) {
+  switch (i) {
+    // if 0 or 3 is the highest, 1 and 2 need to be in the middle
+    case 0:
+    case 3:
+    servos[1].write(heights[1]);
+    servos[2].write(heights[1]);
+    break;
+    // same but in reversed
+    case 1:
+    case 2:
+    servos[0].write(heights[1]);
+    servos[3].write(heights[1]);
+    break;
+  }
+
+  // find the oppisite motor and set it to the highest stand
+  int opposite_i = 0;
+  for (auto v: opposites) {
+    if (i == opposites[i][0]) {
+      opposite_i = opposites[i][1];
+    } else if (i == opposites[i][1]) {
+      opposite_i = opposites[i][0];
+    }
+  }
+  servos[opposite_i].write(heights[2]);
+}
+
+// the function name sais enough
+int highest_element_index(int arr[ldr_count][2]) {
   int highest_i = 0;
   int highest_v = arr[0][1];
 
-  for (int i = 0; i < senservcount; i++) {
+  for (int i = 0; i < ldr_count; i++) {
     if (arr[i][1] > highest_v) {
       highest_i = arr[i][0];
       highest_v = arr[i][1];
